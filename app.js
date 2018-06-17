291516e189b4a2624649a9ee61628027bcf69053
Nicholas Lishchinskyi <nl@letyshops.ru>
2018-06-17 14:44:20 +0300

    init

---
+++ app.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const router = express.Router();
+const app = express()
+const uristring = process.env.MONGODB_URI || 'mongodb://localhost/ExpertSystem';
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+router.use(express.static(__dirname + '/assets'))
+
+mongoose.connect(uristring, (err, res) => {
+    if (err) {
+        console.log('ERROR connecting to: ' + uristring + '. ' + err);
+    } else {
+        console.log('Succeeded connected to: ' + uristring);
+    }
+});
+
+const options = {
+    host: 'localhost',
+    port: 8080
+}
+
+// Create a server with a host and port
+app.listen(options.port, () => console.log(`Example app listening on port ${options.port}!`))
+
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/views/index.html')
+})
